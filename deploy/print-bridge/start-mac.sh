0bb054cc6143de0e007c28751958c4f9aea157f7
Alavont Deploy <deploy@alavont.com>
2026-05-23 04:33:40 -0700

    Force Mac print bridge label queue

--- deploy/print-bridge/start-mac.sh
+++ deploy/print-bridge/start-mac.sh
@@ -5,7 +5,7 @@
 cd "$(dirname "$0")"
 
 export PRINT_BRIDGE_API_KEY="YsFqy1xcWb0lS8arJw0T97qO6mEVM8USemwpqpP5AML"
-export PRINTER_NAME="${PRINTER_NAME:-Label_Themal_Printer}"
+export PRINTER_NAME="Label_Themal_Printer"
 export CUPS_RAW="${CUPS_RAW:-0}"
 unset DIRECT_PRINTER_IP
 unset DIRECT_PRINTER_PORT
